44137ab9f3c7ab382b186ae29d5a51cc5a19c922
justbo <b.tsybulskyy@gmail.com>
2017-11-07 15:53:38 +0200

    s4l15

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Todolist</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+    <footer class="text-center">
+      <p>Copyright &copy; 2017</p>
+    </footer>
+    @section('javascript')
+      <script type="text/javascript" src="/js/app.js"></script>
+      @show
+  </body>
+</html>
